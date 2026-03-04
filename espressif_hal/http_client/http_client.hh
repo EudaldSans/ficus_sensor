989d0cbd5777b5732443dd656796cbe44d244fe0
Eudald Sans <eudald.sans@gmail.com>
2026-03-04 07:21:29 +0100

    defined speciffic espressif http layer

--- espressif_hal/http_client/http_client.hh
+++ espressif_hal/http_client/http_client.hh
@@ -1,8 +1,32 @@
 #ifndef HTTP_CLIENT_HH
 #define HTTP_CLIENT_HH
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
 
+#include "esp_http_client.h"
 
+#include "http_hal.hh"
 
+class HttpClient : public IHttpClient {
+public:
+    ~HttpClient() {if (_initialized) deinit();}
+
+    fic_error_t init();
+    fic_error_t deinit();
+
+    fic_error_t patch(const Request& req, Response& resp, char* rx_buffer, size_t rx_size);
+    fic_error_t post(const Request& req, Response& resp, char* rx_buffer, size_t rx_size);
+    fic_error_t get(const Request& req, Response& resp, char* rx_buffer, size_t rx_size);
+    fic_error_t del(const Request& req, Response& resp, char* rx_buffer, size_t rx_size);
+    fic_error_t put(const Request& req, Response& resp, char* rx_buffer, size_t rx_size);
+
+private:
+    esp_err_t _event_handler(esp_http_client_event_t *evt);
+
+    bool _initialized = false;
+
+};
 
 #endif
