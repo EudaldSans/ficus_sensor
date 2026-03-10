6aa1c7788ae822cdd6fc73cc5564bcf2d64e317d
Eudald <eusa@hms-networks.com>
2026-03-10 10:47:49 +0100

    document http client children

--- espressif_hal/http_client/http_plain_client.hh
+++ espressif_hal/http_client/http_plain_client.hh
@@ -5,6 +5,11 @@
 
 class PlainHttpClient : public HttpClient {
 protected:
+    /**
+     * @brief Just sets transport type to TCP, no TLS support
+     * 
+     * @param config @c esp_http_client_config_t to complement client configuration
+     */
     void _configure_client(esp_http_client_config_t& config) override {
         config.transport_type = HTTP_TRANSPORT_OVER_TCP;
     }
