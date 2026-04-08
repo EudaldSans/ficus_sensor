2fe0916b43b9c7e7a2a8f98311e11df05269f08c
Eudald <eusa@hms-networks.com>
2026-04-08 13:46:28 +0200

    added versions to all project components

---
+++ firebase_stack/firebase_stack_version.hh
@@ -0,0 +1,8 @@
+#ifndef FIREBASE_STACK_VERSION_HH
+#define FIREBASE_STACK_VERSION_HH
+
+#include "version.hh"
+
+static constexpr Version firebase_stack_version = Version(0, 0, 1, 0);
+
+#endif
---
+++ freertos_hal/freertos_hal_version.hh
@@ -0,0 +1,8 @@
+#ifndef FREERTOS_HAL_VERSION_HH
+#define FREERTOS_HAL_VERSION_HH
+
+#include "version.hh"
+
+static constexpr Version freertos_hal_version = Version(0, 0, 1, 0);
+
+#endif
