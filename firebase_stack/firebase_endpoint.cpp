f591b9a3069e8970e6fb3b83f467a8132181b2a9
Eudald Sans <eudald.sans@gmail.com>
2026-06-28 00:09:30 +0200

    force firebase endpoint to report on oclock values

--- firebase_stack/firebase_endpoint.cpp
+++ firebase_stack/firebase_endpoint.cpp
@@ -6,15 +6,39 @@
 FirebaseEndpoint::~FirebaseEndpoint() {}
 
 void FirebaseEndpoint::setup() {
-    return;
+    bool periodic = true;
+
+    _emit_timeout.start(2000, periodic);
 }
 
 void FirebaseEndpoint::update(uint32_t now) {
     static bool send_data = false;
 
+    if (!_emit_timeout.has_expired()) { return; }
     if (_wifi_manager.get_state() != WiFiState::STA_CONNECTED) { return; }
     if (!_sntp_client.is_synced()) { return; }
-    if (!_emit_timeout.has_expired()) { return; }
+
+    if (_sync_state == no_sync) {
+        struct timeval tv;
+        
+        gettimeofday(&tv, NULL);
+        long long time_in_ms = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+
+        uint32_t time_since_synchronization = time_in_ms % _emit_period_ms;
+        uint32_t time_to_next_synchronization = _emit_period_ms - time_since_synchronization;
+
+        FIC_LOGI(TAG, "Syncing report time: Time since sync %d, time to sync %d", time_since_synchronization, time_to_next_synchronization);
+
+        _emit_timeout.update_duration(time_to_next_synchronization);
+        _sync_state = first_sync;
+
+        return;
+
+    } else if (_sync_state == first_sync) {
+        FIC_LOGI(TAG, "First sync completed");
+        _emit_timeout.update_duration(_emit_period_ms);
+        _sync_state = full_sync;
+    }
 
     send_data = false;
     
@@ -26,6 +50,7 @@ void FirebaseEndpoint::update(uint32_t now) {
             using T = std::decay_t<decltype(value)>;
 
             if constexpr (std::is_same_v<T, float>) {
+                value = (int)(value * 100 + 0.5) / 100.0;
                 _firebase_encoder.update_data<float>(value, channel->name.data());
                 FIC_LOGI(TAG, "Evaluated channel %.*s to %f", (int)channel->name.size(), channel->name.data(), value);
             } else if constexpr (std::is_same_v<T, bool>) {
